1745aba4af83a8aaaf07d717039ea3e02583d7ac
sk-mk2 <ee57042@meiji.ac.jp>
2018-07-27 22:47:08 +0900

    logger使ってみた

--- express/app.js
+++ express/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+// log4jsの読み込み
+const log4js  = require('log4js');
+// ログ出力設定
+log4js.configure('./log4js.config.json');  
+// ロガーの生成
+const logger = log4js.getLogger('system');
 
+app.use(log4js.connectLogger(logger, {level: 'auto'}));
 app.use(express.static(__dirname + '/public'));
 
 const {ip, port} = require('./env.json');
 const PORT = process.env.port || port;
 app.listen(PORT, () => {
-    console.log(`IP: ${ip}`);
-    console.log(`App listening on port ${PORT}`);
-    console.log('Press Ctrl+C to quit');
+    logger.info(`IP: ${ip}`);
+    logger.info(`App listening on port ${PORT}`);
+    logger.info('Press Ctrl+C to quit');
 });
+
