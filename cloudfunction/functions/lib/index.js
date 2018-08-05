5a2d489a40986448de447f6b2403ff4986c27e3d
sk-mk2 <ee57042@meiji.ac.jp>
2018-08-05 15:10:52 +0900

    ランダムな数字を返す関数をデプロイ

--- cloudfunction/functions/lib/index.js
+++ cloudfunction/functions/lib/index.js
@@ -4,7 +4,12 @@ const functions = require("firebase-functions");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.random = functions.https.onRequest((request, response) => {
+    console.log(request.query);
+    const num = request.query.num;
+    response.send(`Random Number is ${getRandomInt(num)}`);
 });
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
 //# sourceMappingURL=index.js.map
--- cloudfunction/functions/src/index.ts
+++ cloudfunction/functions/src/index.ts
@@ -3,6 +3,12 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+export const random = functions.https.onRequest((request, response) => {
+    console.log(request.query);
+    const num:number = request.query.num;
+    response.send(`Random Number is ${getRandomInt(num)}`);
 });
+
+function getRandomInt(max:number):number {
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
